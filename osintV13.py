2bcca31fd50d3f1345f20166e46ed0d6131fb5d6
ExPlo73r <139595447+ExPlo73r@users.noreply.github.com>
2023-07-17 01:01:55 -0400

    Update osintV13.py

--- osintV13.py
+++ osintV13.py
@@ -6,21 +6,18 @@ import sources.sii as sii
 import sources.volanteomaleta as volanteomaleta
 import sources.numverify as numverify
 import sources.masterchileapkBday as mchaBday
-
-parser = argparse.ArgumentParser(prog='Osint By Viernez13', description='Busqueda automatica en fuentes abiertas (y no tan abiertas) de chile')
-parser.add_argument('-rut',  type=str, nargs='?', help='Rut de la persona a buscar, con formato: 11111111-1 ')
-parser.add_argument('-patente', type=str, nargs='?', help='Patente del vehiculo a buscar, con formato: aabb11')
-parser.add_argument('-telefono', type=str, nargs='?', help='telefono a buscar, con formato: 56999999999')
-
-parametros = parser.parse_args()
-
-
  print('██╗░░░██╗██╗███████╗██████╗░███╗░░██╗███████╗███████╗░░███╗░░██████╗░')
  print('██║░░░██║██║██╔════╝██╔══██╗████╗░██║██╔════╝╚════██║░████║░░╚════██╗')
  print('╚██╗░██╔╝██║█████╗░░██████╔╝██╔██╗██║█████╗░░░░███╔═╝██╔██║░░░█████╔╝')
  print('░╚████╔╝░██║██╔══╝░░██╔══██╗██║╚████║██╔══╝░░██╔══╝░░╚═╝██║░░░╚═══██╗')
  print('░░╚██╔╝░░██║███████╗██║░░██║██║░╚███║███████╗███████╗███████╗██████╔╝')
  print('░░░╚═╝░░░╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░╚══╝╚══════╝╚══════╝╚══════╝╚═════╝░')
+parser = argparse.ArgumentParser(prog='Osint By Viernez13', description='Busqueda automatica en fuentes abiertas (y no tan abiertas) de chile')
+parser.add_argument('-rut',  type=str, nargs='?', help='Rut de la persona a buscar, con formato: 11111111-1 ')
+parser.add_argument('-patente', type=str, nargs='?', help='Patente del vehiculo a buscar, con formato: aabb11')
+parser.add_argument('-telefono', type=str, nargs='?', help='telefono a buscar, con formato: 56999999999')
+
+parametros = parser.parse_args()
 
 if not(parametros.rut) and  not(parametros.patente) and  not(parametros.telefono):
     parser.print_help()
